71ddc9de57b50c224b29f5d1850b413301b69eb2
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-02-06 00:52:22 -0700

    update

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,7 +1,93 @@
+import "dotenv/config";
 import express from "express";
 const app = express();
+// Loggind api
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const port = 3000;
+app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
+let teaData = [];
+let nextId = 1;
+
+//add new tea
+app.post("/teas", (req, res) => {
+  const { name, price } = req.body;
+  const newTea = { id: nextId++, name, price };
+  teaData.push(newTea);
+  res.status(201).send(newTea);
+});
+
+//get all teas
+app.get("/teas", (req, res) => {
+  res.status(200).send(teaData);
+});
+
+//get a tea with id
+app.get("/teas/:id", (req, res) => {
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("tea not found");
+  }
+
+  res.status(200).send(tea);
+});
+
+//update tea
+app.put("/teas/:id", (req, res) => {
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("tea not found");
+  }
+  console.log(tea);
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+  res.send(200).send(tea);
+});
+
+//delete tea
+app.delete("/teas/:id", (req, res) => {
+  const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("Tea not found");
+  }
+  console.log(index);
+  teaData.splice(index, 1);
+  return res.status(204).send("Deleted");
+});
+
+// app.get("/", (req, res) => {
+//   res.send("Hello you reach the home page");
+// });
+
+// app.get("/ice-tea", (req, res) => {
+//   res.send("Wow ice cream");
+// });
+
+// app.get("/twitter", (req, res) => {
+//   res.send("hello my twitter id is supermand");
+// });
+
 app.listen(port, () => {
-  console.log("Hello world");
+  console.log(`Server is running at ${port}`);
 });
